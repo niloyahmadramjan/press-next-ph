5417c996b88854fb0c6c23e882f3f0ce2276ec1b
RAMJAN <niloyahmadramjan@gmail.com>
2026-07-24 12:51:13 +0800

    created pending state and toaster

--- app/(auth)/_action/authAction.ts
+++ app/(auth)/_action/authAction.ts
@@ -1,19 +1,28 @@
-"use server"
-export const loginAction = async(,formData: FormData)=>{
-    const email = formData.get("email") as string
-    const password = formData.get("password") as string
+"use server";
 
-    const res = await fetch(`${process.env.BACKEND_URL}/api/auth/login`,{
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-    })
-    const result = await res.json()
-    console.log(result)
-    return result
-}
-
+type LoginState = {
+  success: boolean;
+  statasCode: number;
+  massage: string;
+  data: {
+    accessToken: string;
+    refreshToken: string;
+  };
+};
+export const loginAction = async (prevState : LoginState, formData: FormData) => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+//   console.log(prevState)
 
 
+  const res = await fetch(`${process.env.BACKEND_URL}/api/auth/login`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  const result = await res.json();
+  console.log(result);
+  return result;
+};
--- app/(auth)/_components/loginForm.tsx
+++ app/(auth)/_components/loginForm.tsx
@@ -3,19 +3,38 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { loginAction } from "../_action/authAction";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
 
 const LoginForm = () => {
-  const [state, action, pedding]= useActionState(loginAction, false)
+  const [state, action, pedding] = useActionState(loginAction, false);
+  useEffect(() => {
+    if (!state) return;
+    if (state.success) {
+      toast.success(state.message || "Login successful");
+    }
+    if (!state.success) {
+      toast.error(state.message || "Login Faild");
+    }
+  }, [state]);
+
   return (
-    <form  action={action}>
-     <Card className="p-5 space-y-4">
-            <Input name="email" type="email" placeholder="Enter Your Email" required />
-            <Input name="password" type="password" placeholder="Enter Your Password" required />
-            <Button type="submit">
-             {pedding? "Submiting...": "submit"}
-            </Button>
-        </Card>
+    <form action={action}>
+      <Card className="p-5 space-y-4">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Enter Your Email"
+          required
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Enter Your Password"
+          required
+        />
+        <Button type="submit">{pedding ? "Submiting..." : "Login"}</Button>
+      </Card>
     </form>
   );
 };
